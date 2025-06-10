453ec538ef8054cc289707dfda14eb3c2a74bcb6
TempestaTorres <142347458+TempestaTorres@users.noreply.github.com>
2025-06-10 20:07:31 +0300

    slider added

---
+++ dist/scripts/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    const owl = $('.owl-carousel');
+    const prev = $('.slider-button.prev');
+    const next = $('.slider-button.next');
+
+    owl.owlCarousel({
+        loop:true,
+        items: 1,
+        smartSpeed: 600,
+        margin:5,
+    });
+
+    prev.on('click', (e) => {
+        e.preventDefault();
+
+        owl.trigger('prev.owl.carousel');
+
+        e.stopPropagation();
+    });
+    next.on('click', (e) => {
+        e.preventDefault();
+
+        owl.trigger('next.owl.carousel');
+
+        e.stopPropagation();
+    });
+});
